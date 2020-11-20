8640e133c6fa04e6b7c6bca9ef4fac234a6b3655
Mylab <labs@onmycrowd.com>
2020-11-19 23:28:52 -0700

    Making necessary changes to run app with figwheel.

---
+++ src/cmanagement/main.cljs
@@ -0,0 +1,21 @@
+(ns cmanagement.main
+  (:require [react]
+            [react-native :as rn]))
+
+(def <> react/createElement)
+
+(defn renderfn [props]
+  (<> rn/View
+      #js {:style #js {:backgroundColor "#FFFFFF"
+                       :flex 1
+                       :justifyContent "center"}}
+      (<> rn/Text
+          #js {:style #js {:color "black"
+                           :textAlign "center"}}
+          (str "HELLO"))))
+
+;; the function figwheel-rn-root must be provided. It will be called by 
+;; react-native-figwheel-bridge to render your application. 
+;; You can configure the name of this function with config.renderFn
+(defn figwheel-rn-root []
+  (renderfn {}))
