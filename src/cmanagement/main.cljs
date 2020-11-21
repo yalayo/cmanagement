28c6a4341d99caec22c92c7af82dafa4e7361045
Mylab <labs@onmycrowd.com>
2020-11-20 22:24:27 -0700

    Using reagent to display message.

--- src/cmanagement/main.cljs
+++ src/cmanagement/main.cljs
@@ -1,21 +1,21 @@
 (ns cmanagement.main
-  (:require [react]
+  (:require [reagent.core :as r]
             [react-native :as rn]))
 
-(def <> react/createElement)
+(defn adapt [class]
+  (r/adapt-react-class class))
 
-(defn renderfn [props]
-  (<> rn/View
-      #js {:style #js {:backgroundColor "#FFFFFF"
-                       :flex 1
-                       :justifyContent "center"}}
-      (<> rn/Text
-          #js {:style #js {:color "black"
-                           :textAlign "center"}}
-          (str "HELLO"))))
+(def safe-area-view (adapt rn/SafeAreaView))
+(def view (adapt rn/View))
+(def text (adapt rn/Text))
+
+(defn app []
+  [safe-area-view {:flex 1}
+   [view {:flex 1 :align-items "center" :justify-content "center"}
+    [text {:style {:font-size 50}} "Hello Jason!"]]])
 
 ;; the function figwheel-rn-root must be provided. It will be called by 
 ;; react-native-figwheel-bridge to render your application. 
 ;; You can configure the name of this function with config.renderFn
 (defn figwheel-rn-root []
-  (renderfn {}))
+  (r/as-element [app]))
