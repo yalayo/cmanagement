4611049eda7da53332d0e78e3406b08aa6a2635e
Mylab <labs@onmycrowd.com>
2020-12-10 22:54:46 -0700

    Move other components to core.

--- src/cmanagement/main.cljs
+++ src/cmanagement/main.cljs
@@ -1,42 +1,17 @@
 (ns cmanagement.main
   (:require [reagent.core :as r]
-            ["react-native" :as rn]
-            ["aws-amplify" :refer (Amplify)]
-            ["@react-navigation/stack" :refer [createStackNavigator]]
-            ["@react-navigation/native" :refer [NavigationContainer]]
             [re-frame.core :as re-frame]
+            [cmanagement.core.components :as c]
             [cmanagement.users.views :as users]
+            [cmanagement.compounds.views :as compounds]
             [cmanagement.users.events :as events]))
 
-(defn adapt [class]
-  (r/adapt-react-class class))
-
-(def stack (createStackNavigator))
-
-(def view (adapt rn/View))
-(def text (adapt rn/Text))
-(def navigator (adapt (.-Navigator stack)))
-(def screen (adapt (.-Screen stack)))
-(def nav-container (adapt NavigationContainer))
-
 (defn app []
-  [nav-container
-   [navigator {:mode :modal :header-mode :none :initial-route-name :sign-in}
-    [screen {:name :sign-in :component (r/reactify-component users/sign-in) :options {:gestureEnabled false}}]
-    [screen {:name :new-password :component (r/reactify-component users/new-password) :options {:gestureEnabled false}}]]])
-
-
-;(defn dash-board []
-;  [safe-area-view {:flexDirection "row" :padding 20}
-;   [view {:flex 1 :align-items "center" :justify-content "center" :borderWidth 1 :borderRadius 10}
-;    [text "TOTAL INGRESOS"]
-;    [text {:style {:font-size 18 :color "blue"}} "$32575.00"]]
-;   [view {:flex 1 :align-items "center" :justify-content "center" :borderWidth 1 :borderRadius 10}
-;    [text "TOTAL GASTOS"]
-;    [text {:style {:font-size 18 :color "blue"}} "$20590.00"]]
-;   [view {:flex 1 :align-items "center" :justify-content "center" :borderWidth 1 :borderRadius 10}
-;    [text "GANANCIA TOTAL"]
-;    [text {:style {:font-size 18 :color "blue"}} "$17100.00"]]])
+  [c/nav-container
+   [c/navigator {:mode :modal :header-mode :none :initial-route-name :sign-in}
+    [c/screen {:name :sign-in :component (r/reactify-component users/sign-in) :options {:gestureEnabled false}}]
+    [c/screen {:name :new-password :component (r/reactify-component users/new-password) :options {:gestureEnabled false}}]
+    [c/screen {:name :register-compound :component (r/reactify-component compounds/register-compound) :options {:gestureEnabled false}}]]])
 
 ;; the function figwheel-rn-root must be provided. It will be called by
 ;; react-native-figwheel-bridge to render your application.
@@ -44,4 +19,3 @@
 (defn figwheel-rn-root []
   (re-frame/dispatch-sync [::events/initialize-db])
     (r/as-element [app]))
-
