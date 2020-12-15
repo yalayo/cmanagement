09f9bdef2d3cf6fc3071a46c4707d28fec661324
Mylab <labs@onmycrowd.com>
2020-12-15 13:14:17 -0700

    Add a spec to check email format.

---
+++ src/cmanagement/users/specs.cljs
@@ -0,0 +1,6 @@
+(ns cmanagement.users.specs
+  (:require [clojure.spec.alpha :as s]))
+
+(def email-regex #"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$")
+(s/def ::email (s/and string? #(re-matches email-regex %)))
+
